8e26c2526afa728e38f5e6241eb323db8935215f
sssbck <zhasabi05@gmail.com>
2026-01-30 05:12:57 +0500

    syntax topic

---
+++ Pr1/syntax.py
@@ -0,0 +1,16 @@
+if 5 > 3:
+    print("Five is greater than three")
+
+x = 5
+y = 3
+print(x + y)
+
+for i in range(3):
+    print(i)
+
+def my_function():
+    print("Hello from function")
+
+my_function()
+
+print("Indentation matters in Python")
