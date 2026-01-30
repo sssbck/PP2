b0f74abdb4e3c52aa1305cd199f7d13230f98641
sssbck <zhasabi05@gmail.com>
2026-01-30 05:16:02 +0500

    comments topic

---
+++ Pr1/comments.py
@@ -0,0 +1,16 @@
+# There hello output code
+print("Hello")
+
+print("Hello")  # Inline comment
+
+# Comment num variable
+x = 10
+print(x)
+
+"""
+exmp for multi-line comment
+"""
+print("Comments example")
+
+# Python dnt care bout cmmnts
+print("Done")
